a66c91ebc5bfca105c66a402131ccb4b7dc53e8d
ASH-TOOLS <113542143+ASH-TOOLS@users.noreply.github.com>
2023-09-28 18:27:29 +0300

    Create smtp_servers.py
    do you look for file that include json info 
    
    "domain":{
    "host":"smtp.example.com",
    "port":"587"
    "secureConnection":"TLS or SSL or TTLS"
    }
    
    yes you  got that have.

---
+++ smtp_servers.py
@@ -0,0 +1,82 @@
+smtp_servers = {
+    'gmail': {
+        'host': 'smtp.gmail.com',
+        'port': 587,
+        'secureConnection': 'STARTTLS'
+    },
+    'yahoo': {
+        'host': 'smtp.mail.yahoo.com',
+        'port': 465,
+        'secureConnection': 'SSL'
+    },
+    'outlook': {
+        'host': 'smtp.office365.com',
+        'port': 587,
+        'secureConnection': 'STARTTLS'
+    },
+    'hotmail': {
+        'host': 'smtp.live.com',
+        'port': 25,
+        'secureConnection': 'None'
+    },
+    'zoho': {
+        'host': 'smtp.zoho.com',
+        'port': 465,
+        'secureConnection': 'SSL'
+    },
+    'sparkpostmail': {
+        'host': 'smtp.sparkpostmail.com',
+        'port': 587,
+        'secureConnection': 'STARTTLS'
+    },
+    'smtp-relay': {
+        'host': 'smtp-relay.sendinblue.com',
+        'port': 587,
+        'secureConnection': 'STARTTLS'
+    },
+    'smtp_aws_ses': {
+        'host': 'email-smtp.us-east-1.amazonaws.com',
+        'port': 587,
+        'secureConnection': 'STARTTLS'
+    },
+    'sendinblue': {
+        'host': 'smtp-relay.sendinblue.com',
+        'port': 587,
+        'secureConnection': 'STARTTLS'
+    },
+    'sendgrid': {
+        'host': 'smtp.sendgrid.net',
+        'port': 587,
+        'secureConnection': 'None'
+    },
+    'mandrill': {
+        'host': 'smtp.mandrillapp.com',
+        'port': 587,
+        'secureConnection': 'STARTTLS'
+    },
+    'mailgun': {
+        'host': 'smtp.mailgun.org',
+        'port': 587,
+        'secureConnection': 'STARTTLS'
+    },
+    'Mailchimp': {
+        'host': 'smtp.mailchimp.com',
+        'port': 587,
+        'secureConnection': 'STARTTLS'
+    },
+    'SocketLabs':{
+        'host': 'smtp.socketlabs.com',
+        'port': 587,
+        'secureConnection': 'STARTTLS'
+    },
+    'SMTP2GO':{
+        'host': 'mail.smtp2go.com',
+        'port': 587,
+        'secureConnection': 'None'
+    },
+    'Moosend':{
+        'host': 'smtp.moosend.com',
+        'port': 587,
+        'secureConnection': 'STARTTLS'
+    }
+}
